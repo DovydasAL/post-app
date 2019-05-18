14811ce8e23e3af44801f2915970c42663eeb9f9
DovydasAL <davidlazauskas1@gmail.com>
2019-05-18 16:08:55 -0400

    add grunt

---
+++ gruntFile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        // Configure nodemon
+        nodemon: {
+            dev: {
+                script: 'server.js'
+            }
+        }
+
+    });
+
+    // Load nodemon
+    grunt.loadNpmTasks('grunt-nodemon');
+
+    // Register the nodemon task when we run grunt
+    grunt.registerTask('default', ['nodemon']);
+
+};
